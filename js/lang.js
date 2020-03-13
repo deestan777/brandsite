976468be5570a6d7a1eacc119d946f65a38b2057
denysov <deestan@ymail.com>
2020-03-13 15:27:23 +0200

    add

--- js/lang.js
+++ js/lang.js
@@ -12,9 +12,70 @@ var arrLang = {
     'ru': 'Russian',
 
     'title':'We help you managing asset, provide financial advise. Leave money issue with us and focus on your core business.',
-    'registration':'Registration'
+    'registration':'Registration',
+
+    'advantages':'Our advantages',
+    'advDescription':'Investing Capital is a Forex broker who provides personalized solutions to clients regardless of their level of knowledge and experience. At Investing Capital, you can start trading right away using any of our efficient platforms. One of which is Meta Trader 4.',
+
+    'col1':'Management accounts',
+    'col11':'At the moment in the management of our company - 12 4555 accounts.',
+    'col2':'Management accounts',
+    'col22':'At the moment in the management of our company - 12 4555 accounts.',
+    'col3':'Management accounts',
+    'col33':'At the moment in the management of our company - 12 4555 accounts.',
+    'col4':'Management accounts',
+    'col44':'At the moment in the management of our company - 12 4555 accounts.',
+    'col5':'Management accounts',
+    'col55':'At the moment in the management of our company - 12 4555 accounts.',
+    'col6':'Management accounts',
+    'col66':'At the moment in the management of our company - 12 4555 accounts.',
+
+    'howDeposit':'How to make a deposit',
+
+    'cool1':'Register',
+    'cool11':'Create your own account. It takes only a few minutes',
+    'cool2':'Confirmation',
+    'cool22':'The system will automatically send a letter to your e-mail',
+    'cool3':'Deposit',
+    'cool33':'The system has a simple and convenient interface.',
+    'cool4':'Trading',
+    'cool44':'Start trading and get profit',
+
+    'quotes':'Quotes',
+
+    'forex':'Forex',
+    'indices':'Indices',
+    'stocks':'Stocks',
+    'pares':'Pares',
+
+    'clients':'All Time Clients',
+    'clientsYear':'Clients In This Year',
+    'projects':'New Projects',
+    'avg':'Avg Profit Increased',
+
+    'callBack':'Request a call back?',
+    'callBackDesc':'Please fill out the form and press the submit button. We will get back to you with 1-2 business days.',
+
+    'info': 'Information and high risk warning:',
+    'infoDesc': 'Trading with foreign exchange (FX), contract for differences (CFDs) and precious metals carries a high level of risk that may not be suitable for all investors. Leverage creates additional risk and loss exposure. Before you decide to trade foreign exchange or contract for differences, carefully consider your investment objectives, experience level, and risk tolerance.',
+    'legal': 'legal disclaimer:',
+    'legalDesc':'Forex trading entails significant risks and is not appropriate for all investors. The possibility of incurring substantial losses should be taken into account. It is therefore important to understand the possible consequences of investing. Traders should weigh their earning potential against the risks involved and act accordingly. Dream Equity Ltd operating under Investing Capital brand and using investingcapital.com domain within the European Economic Area.',
+
+    'owned':'This site is owned Dream Equity Ltd Registration number : 205267596 Incorporated – September 10 2018 Residence and management address : 1000 Sofia City, Sofia District (Metropolis); Metropolitan Municipality, Vuzrazhdane Region, 147 Knyaz Boris I Str., app.12',
+
+    'copyright':'Copyright 2020, Interactive Trade All Right Reserved',
+
+    'tac':'Terms and Conditions ',
+    'pp' :'Privacy Policy ',
+    'rw' :'Risk Warnings ',
+    'aml':'Anti-Money Laundering'
 
   },
+
+
+
+
+
   'ru': {
     'home': 'Главная',
     'about': 'О нас',
@@ -28,7 +89,63 @@ var arrLang = {
     'ru': 'Русский',
 
     'title':'Доверьте нам свои деньги и сфокусируйтесь на вашем бизнесе.',
-    'registration':'Регистрация'
+    'registration':'Регистрация',
+
+    'advantages':'Наши преимущества',
+    'advDescription':'инвестиционный капитал - брокер форекс, который предлагает клиентам персонализованные решения в отношении их уровня знаний и опыта. в инвестирующем капитале вы можете начать торговли прямо использовать любой нашей эффективной платформой. один из мета трейдер 4.',
+
+    'col1':'Управленческий учет',
+    'col11':'На данный момент в управлении нашей компании - 12 4555 счетов.',
+    'col2':'Управленческий учет',
+    'col22':'На данный момент в управлении нашей компании - 12 4555 счетов.',
+    'col3':'Управленческий учет',
+    'col33':'На данный момент в управлении нашей компании - 12 4555 счетов.',
+    'col4':'Управленческий учет',
+    'col44':'На данный момент в управлении нашей компании - 12 4555 счетов.',
+    'col5':'Управленческий учет',
+    'col55':'На данный момент в управлении нашей компании - 12 4555 счетов.',
+    'col6':'Управленческий учет',
+    'col66':'На данный момент в управлении нашей компании - 12 4555 счетов.',
+
+    'howDeposit':'Как сделать депозит',
+
+    'cool1':'Регистр',
+    'cool11':'Создайте свою учетную запись. Это займет всего несколько минут.',
+    'cool2':'Подтверждение',
+    'cool22':'Система автоматически отправит письмо на вашу электронную почту.',
+    'cool3':'Депозит',
+    'cool33':'Система имеет простой и удобный интерфейс.',
+    'cool4':'Трэйдинг',
+    'cool44':'Начни торговать и получай прибыль.',
+
+    'quotes':'Катировки',
+
+    'forex':'Форекс',
+    'indices':'Индексы',
+    'stocks':'Акции',
+    'pares':'Пары',
+
+    'clients':'Наши клиенты',
+    'clientsYear':'Клиентов за год',
+    'projects':'Новые проэкты',
+    'avg':'Среднее увеличение депозита',
+
+    'callBack':'Заказать обратный звонок?',
+    'callBackDesc':'Пожалуйста, заполните форму и нажмите кнопку отправки. Мы свяжемся с вами в течение 1-2 рабочих дней.',
+
+    'info': 'Информация и предупреждение о высоком риске:',
+    'infoDesc': 'Торговля с иностранной валютой (FX), контрактом на разницу (CFD) и драгоценными металлами несет высокий уровень риска, который может не подходить для всех инвесторов. Кредитное плечо создает дополнительный риск и экспозицию потери. Прежде чем принять решение об обмене иностранной валюты или заключить контракт на разницу, тщательно продумайте свои инвестиционные цели, уровень опыта и допустимость рисков.',
+    'legal':'Правовая оговорка:',
+    'legalDesc':'Торговля на Форекс влечет за собой значительные риски и подходит не всем инвесторам. Возможность понести существенные потери должны быть приняты во внимание. Поэтому важно понимать возможные последствия инвестирования. Трейдеры должны взвесить свой потенциальный доход с учетом рисков и действовать соответственно. Dream Equity Ltd работает под брендом Investing Capital и использует домен investingcapital.com в рамках Европейского экономического пространства.',
+
+    'owned':'Сайт принадлежит Dream Equity Ltd Регистрационный номер: 205267596 Зарегистрировано - 10 сентября 2018 г. Адрес проживания и управления: 1000 София, Софийский район (Метрополис); Столичный муниципалитет, Вузраждане, Князь Борис I, 147, ок.12',
+
+    'copyright':'Copyright 2020, Interactive Trade Все права защищены',
+
+    'tac':'Условия использования ',
+    'pp' :'Политика конфиденциальности ',
+    'rw' :'Предупреждения о рисках ',
+    'aml':'Борьба с отмыванием денег'
 
   }
 };
@@ -39,7 +156,7 @@ $(function() {
     var lang = $(this).attr('id');
 
     $('.lang').each(function(index, item) {
-      $(this).text(arrLang[lang][$(this).attr('key')]);
+      $(this).text(arrLang[lang][$(this).attr('key')] ) ;
     });
   });
 });
